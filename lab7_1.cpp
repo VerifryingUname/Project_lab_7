77820506dbc402543a748b426e9e46ea373a5099
VerifryingUname <kingdagone1@gmail.com>
2024-12-03 15:45:21 +0700

    lab7.1.cpp completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    float a  =  20, b = 30;
+    float c = 4;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    float d = a - b/c;
+    cout << "output: " << d << '\n';
     return 0;
 }
